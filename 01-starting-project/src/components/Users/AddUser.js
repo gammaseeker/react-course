c93d8e6821c1d3cad9f82b15bdccabd2331e79ed
gammaseeker <joeyjiem@gmail.com>
2021-07-19 16:20:24 -0400

    Added Wrapper component to clean up HTML

--- 01-starting-project/src/components/Users/AddUser.js
+++ 01-starting-project/src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModel";
+import Wrapper from "../Helpers/Wrapper";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
@@ -13,17 +14,17 @@ const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
-        setError({
-            title: 'Invalid input',
-            message: 'Please enter a valid name and age (non-empty values).'
-        });
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
     if (+enteredAge <= 1) {
-        setError({
-            title: 'Invalid age',
-            message: 'Please enter a valid age (> 0).'
-        });
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -40,12 +41,18 @@ const AddUser = (props) => {
   };
 
   const errorHandler = () => {
-      setError(null);
-  }
+    setError(null);
+  };
 
   return (
-    <div>
-      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
+    <Wrapper>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
@@ -65,7 +72,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
